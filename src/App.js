7e5cff655cbcfa2f7e28558817dffeac0bc617ed
Linykeer Almeida <linykeerrenato@gmail.com>
2021-01-10 06:13:16 -0300

    Feature: finished clone interface netflix

--- src/App.js
+++ src/App.js
@@ -3,27 +3,67 @@ import React,{useEffect, useState} from 'react';
 import Tmdb from './Tmdb';
 import './App.css';
 import MovieRow from './component/MovieRow'
+import FeaturedMovie from './component/FeaturedMovie'
+import Header from './component/Header'
 
 export default () => {
-const [movieList, setMovieList] = useState([])
+const [movieList, setMovieList] = useState([]);
+const [featuredData,setFeaturedData] = useState(null);
+const [backHeader, setBlackHeader] = useState(false);
+
 
   useEffect(() => {
     const loadAll = async () => {
     let list = await Tmdb.getHomeList();
-    console.log(list)
     setMovieList(list)
+
+     let originals = list.filter(i => i.slug === 'originals');
+     let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1))
+     let chosen = originals[0].items.results[randomChosen];
+    let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+     setFeaturedData(chosenInfo);
+     
     }
 
     loadAll();
   },[])
 
+  useEffect(() => {
+    const scrollListener =() => {
+        if(window.scrollY > 50){
+        setBlackHeader(true)
+        } else {
+          setBlackHeader(false)
+        }
+    }
+    
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+
+    }
+  },[])
+
   return (
     <div className="page">
+      <Header black={backHeader}/>
+      {featuredData &&  <FeaturedMovie item={featuredData}/>}
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items}/>
         ))}
       </section>
+
+      <footer>
+        Feito por Linykeer Almeida<br/>
+        Direitos de imagem para Netflix<br/>
+        Dados pegos do site Themoviedb.org
+      </footer>
+      {movieList.length <= 0 &&
+      <div className="loading">
+        <img src="https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_560%2Cc_limit/Netflix_LoadTime.gif" alt="Carregando" />
+      </div>
+      }
     </div>
   )
 }
